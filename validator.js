8750ee9eec86b212085ad61db01a494d3bc35add
fabian.olsthoorn <fabian.olsthoorn@rws.nl>
2023-11-02 11:07:30 +0100

    Attempts to fix file ownership issues

--- validator.js
+++ validator.js
@@ -1,7 +1,22 @@
 function validateForm() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
+/*
+    const query = "SELECT * FROM mytable WHERE name = ? AND pass = ?";
 
+    db.run(query, [username, password], (err, row) => {
+        if (err) {
+            console.error("error!");
+        } else {
+            if (row) {
+                console.log(`User with name '${username}' and password '${password}' exists.`);
+            } else {
+                console.log(`User not found or password is incorrect.`);
+            }
+        }
+    });
+    db.close();
+*/
     if (username === "user" && password === "user") {
         window.location.href = "userpage.html?userID=2";
     } else {
