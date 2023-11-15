a3f43207af7bd746e94bb306ed02e176a5c67216
fabian.olsthoorn <fabian.olsthoorn@rws.nl>
2023-11-15 10:19:16 +0100

    changed to accommodate changes

--- Documents/startup.sh
+++ Documents/startup.sh
@@ -2,28 +2,31 @@
 
 # General escaperoom setup
 chmod -R 770 ../../../home/*
-chown room2:room2 ../../../home/room2/
-chown room2:room2 ../../../home/room2/*
-chown room3:room3 ../../../home/room3/
-chown room3:room3 ../../../home/room3/*
-chown room4:room4 ../../../home/room4/
-chown room4:room4 ../../../home/room4/*
-chown room5:room5 ../../../home/room5/
-chown room5:room5 ../../../home/room5/*
+chown room2:room2 ../../../home/room3.gpg
+chown room2:room2 ../../../home/room4.gpg
+chown room2:room2 ../../../home/room5.gpg
+#chown room2:room2 ../../../home/room2/
+#chown room2:room2 ../../../home/room2/*
+#chown room3:room3 ../../../home/room3/
+#chown room3:room3 ../../../home/room3/*
+#chown room4:room4 ../../../home/room4/
+#chown room4:room4 ../../../home/room4/*
+#chown room5:room5 ../../../home/room5/
+#chown room5:room5 ../../../home/room5/*
 
 # Room 3 setup
 chown room3:room3 ../../../usr/bin/find
 chmod 770 ../../../usr/bin/find
 
 # Room 4 setup
-chown admin:admin ../../../home/room4/Documents/flag4.txt
+#chown admin:admin ../../../home/room4/Documents/flag4.txt
 chown admin:admin ../../../bin/less
 chmod u+s ../../../bin/less
 
 # Room 5 setup
-chown admin:admin ../../../home/room5/Documents/script.sh
-chmod 774 ../../../home/room5/Documents/script.sh
-chmod u+s ../../../home/room5/Documents/script.sh
+#chown admin:admin ../../../home/room5/Documents/script.sh
+#chmod 774 ../../../home/room5/Documents/script.sh
+#chmod u+s ../../../home/room5/Documents/script.sh
 
 # Remove evidence
 rm ../../../home/room2/room2startup.sh
