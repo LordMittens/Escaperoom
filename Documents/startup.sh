3eebf5dad3e693afb121c4bee5acd727b0112f99
fabian.olsthoorn <fabian.olsthoorn@rws.nl>
2023-11-24 11:45:26 +0100

    Forgot file with last push

--- Documents/startup.sh
+++ Documents/startup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # General escaperoom setup
-chown -R admin:admin ../../../home/admin/
 chown -R room2:room2 ../../../home/room2/
 chown -R room3:room3 ../../../home/room3/
 chown -R room4:room4 ../../../home/room4/
@@ -9,13 +8,14 @@ chown -R room5:room5 ../../../home/room5/
 
 # Room 4 setup
 chown room4:room4 ../../../usr/bin/find
+chmod 750 ../../../usr/bin/find
 chmod 4751 ../../../usr/bin/galf
 
 # Room 5 setup
 chmod 770 ../../../home/room5/script.sh
 chmod 754 ../../../usr/bin/unzip
 chmod 755 ../../../usr/bin/sus
-zip ../../../home/room5/flag5.zip ../../../home/room5/flag5.txt
+zip -q -j -P ZmxhZzUK ../../../home/room5/flag5.zip ../../../home/room5/flag5.txt
 rm ../../../home/room5/flag5.txt
 
 # Remove evidence
