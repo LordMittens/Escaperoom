5566756b67147b1d44af81380db830c027283814
Fabian Olsthoorn <17079292@student.hhs.nl>
2023-12-08 13:45:54 +0100

    Added a flag form to the start screen and added more default credentials to the first vulnerability.

--- validator.js
+++ validator.js
@@ -2,7 +2,8 @@ function validateForm() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
-    if (username === "user" && password === "user") {
+    if ((username === "user" || username === "username" || username === "gebruiker") && 
+        (password === "user" || password === "username" || password === "password" || password === "1234")) {
         window.location.href = "userpage.html?userID=2";
     } else {
         alert("Invalid username or password. Please try again.");
