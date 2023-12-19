4f745e5810202fb25e9d2129c469d7f11683c223
fabian.olsthoorn <fabian.olsthoorn@rws.nl>
2023-12-19 13:53:24 +0100

    Small visual changes, added passwords for flag1.

--- validator.js
+++ validator.js
@@ -3,7 +3,7 @@ function validateForm() {
     var password = document.getElementById("password").value;
 
     if ((username === "user" || username === "username" || username === "gebruiker") && 
-        (password === "user" || password === "username" || password === "password" || password === "1234")) {
+        (password === "user" || password === "username" || password === "password" || password === "wachtwoord" || password === "1234" || password === "123456" )) {
         window.location.href = "userpage.html?userID=2";
     } else {
         alert("Invalid username or password. Please try again.");
